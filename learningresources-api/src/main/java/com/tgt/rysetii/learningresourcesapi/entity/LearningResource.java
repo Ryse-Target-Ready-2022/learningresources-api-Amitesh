44e75179695f06d4abf05b37f7d20b6719c2226a
amitesh kumar singh <amiteshkumarsingh01@gmail.com>
2022-07-12 14:40:52 +0530

    constructors and get set methods created

--- learningresources-api/src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
+++ learningresources-api/src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
@@ -25,6 +25,70 @@ public class LearningResource {
         this.publishedDate=publishedDate;
         this.retiredDate=retiredDate;
     }
+
+    public Integer getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Integer resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public Double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public LearningResourceStatus getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(LearningResourceStatus productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public LocalDate getRetiredDate() {
+        return retiredDate;
+    }
+
+    public void setRetiredDate(LocalDate retiredDate) {
+        this.retiredDate = retiredDate;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
 
 
