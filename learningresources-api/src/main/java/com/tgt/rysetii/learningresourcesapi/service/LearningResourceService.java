8ebb713306572b052728fdd0de221ad5ee4c2b3c
amitesh kumar singh <amiteshkumarsingh01@gmail.com>
2022-07-12 16:59:35 +0530

    Add method to save data to csv and also sort by profit margin

--- learningresources-api/src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
+++ learningresources-api/src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
@@ -8,8 +8,42 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 public class LearningResourceService {
+
+    public void saveLearningResources(List<LearningResource> learningResources){
+        populateLearningResourcesToCsv(learningResources);
+    }
+
+    public List<LearningResource> getLearningResources(){
+        File learningResourcesFile=new File("LearningResources.csv");
+        List<LearningResource> learningResources=loadLearningResourceFromCsv(learningResourcesFile);
+        return learningResources;
+    }
+
+    public List<Double> getProfitMargin(){
+        List<LearningResource> learningResources=getLearningResources();
+
+        List<Double> profitMargins=learningResources.stream()
+                .map(lr -> ((lr.getSellingPrice()- lr.getCostPrice())/lr.getSellingPrice()))
+                .collect(toList());
+
+        return profitMargins;
+    }
+
+    public List<LearningResource> sortLearningResourcesByProfitMargin(){
+        List<LearningResource> learningResources= getLearningResources();
+
+        learningResources.sort((lr1, lr2) -> {
+            Double profitMargin1 = (lr1.getSellingPrice()-lr1.getCostPrice())/ lr1.getSellingPrice();
+            Double profitMargin2 = (lr2.getSellingPrice()- lr2.getCostPrice())/ lr2.getSellingPrice();
+
+            return profitMargin2.compareTo(profitMargin1);
+        });
+
+        return learningResources;
+    }
     private List<LearningResource> loadLearningResourceFromCsv(File csvFile){
         List<LearningResource> learningResources= new ArrayList<>();
         try {
